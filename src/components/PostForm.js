ce4285b1511fe429070e7ade0c74477f35a37958
jeremyjgyoung <jeremyjgyoung@gmail.com>
2017-11-21 10:50:30 -0800

    turned the PostForm.js into a controlled component with its own state

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 
 // Be able to post
 class PostForm extends Component {
+
+  state = { typedPost: '' }
+  handleChange = (event) => {
+    this.setState({typedPost: event.target.value.trim() })
+  }
+
   render() {
     return (
       <div className="PostForm">
         <form className="Form">
-          <input type="text" className="Input" />
+          <form>
+            <input type="text" className="Input"
+              value={this.state.email}
+              onChange={this.handleChange}
+             />
+          </form>
           <button type="submit" className="Post-button">POST</button>
         </form>
       </div>
