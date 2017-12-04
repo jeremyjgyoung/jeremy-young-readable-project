b9db29301af309d14b0e2e35c1c7ee78474191cf
jeremyjgyoung <jeremyjgyoung@gmail.com>
2017-12-04 15:11:42 -0800

    added modal to add post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addPost } from '../actions'
+import Modal from 'react-modal'
 
 // Clean up handlers, if possible
 class PostForm extends Component {
@@ -8,7 +9,8 @@ class PostForm extends Component {
     author: '',
     title: '',
     typedPost: '',
-    category: ''
+    category: '',
+    postModalIsOpen: false,
   }
   handleChangeAuthor = (event) => {
     this.setState({author: event.target.value })
@@ -25,41 +27,85 @@ class PostForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault()
   }
+  openPostModal = () => {
+    this.setState({postModalIsOpen: true})
+  }
+  closePostModal = () => {
+    this.setState({postModalIsOpen: false})
+  }
+  onPost = () => {
+    this.setState({postModalIsOpen: false})
+    this.props.addPost({author:this.state.author, title:this.state.title, body:this.state.typedPost, category:this.state.category })
+  }
   render() {
     return (
       <div className="PostForm">
-        <form onSubmit={this.handleSubmit} className="Form">
-            <input type="text" className="Input-name"
-              placeholder="your name"
-              value={this.state.author}
-              onChange={this.handleChangeAuthor}
-             />
-             <input type="text" className="Input-title"
-               placeholder="title"
-               value={this.state.title}
-               onChange={this.handleChangeTitle}
-              />
-            <textarea className="Input-post"
-              placeholder="post"
-              value={this.state.typedPost}
-              onChange={this.handleChangePost}
-             />
-             <div className="Post-cat-post">
-               <select className="Select-category"
-                 value={this.state.category}
-                 onChange={this.handleChangeCategory}>
-                  <option value="">Choose Category</option>
-                  <option value="react">React</option>
-                  <option value="redux">Redux</option>
-                  <option value="read">Udacity</option>
-               </select>
-               <button className="Post-button" onClick={() =>
-                 this.props.addPost({author:this.state.author, title:this.state.title, body:this.state.typedPost, category:this.state.category, })}
-               >POST</button>
-            </div>
-        </form>
+        <button
+            className='Button-open-post-modal'
+            onClick={this.openPostModal}>
+              Add Post
+        </button>
+        <Modal
+          className='modal'
+          overlayClassName='overlay'
+          isOpen={this.state.postModalIsOpen}
+          style={customStyles}
+          contentLabel='Post Modal'
+        >
+          <button onClick={this.closePostModal}>close</button>
+          <form onSubmit={this.handleSubmit} className="Form">
+              <input type="text" className="Input-name"
+                placeholder="your name"
+                value={this.state.author}
+                onChange={this.handleChangeAuthor}
+               />
+               <input type="text" className="Input-title"
+                 placeholder="title"
+                 value={this.state.title}
+                 onChange={this.handleChangeTitle}
+                />
+              <textarea className="Input-post"
+                placeholder="post"
+                value={this.state.typedPost}
+                onChange={this.handleChangePost}
+               />
+               <div className="Post-cat-post">
+                 <select className="Select-category"
+                   value={this.state.category}
+                   onChange={this.handleChangeCategory}>
+                    <option value="">Category</option>
+                    <option value="react">React</option>
+                    <option value="redux">Redux</option>
+                    <option value="read">Udacity</option>
+                 </select>
+                 <button className="Post-button" onClick={this.onPost}>
+                   POST
+                 </button>
+              </div>
+          </form>
+        </Modal>
       </div>
     )
+  }
+}
+
+const customStyles = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.50)'
+  },
+  content : {
+    position              : 'fixed',
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
   }
 }
 
